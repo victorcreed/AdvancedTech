7ef9a43d5ade2d5ee3bf30af4f3d6f63b47c139b
Victor Creed <a2ninek@yahoo.com>
2012-02-25 23:41:36 +0500

    Bug Fixes

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,8 @@
+class HomeController < ApplicationController
+  def index
+    flash[:alert] = "This site is underconstruction"
+    
+
+  end
+
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AdvancedTech2::Application.config.secret_token = 'ae46e1b6fb8cfb4eff2aa5547940aca22b50b3f77e4de507ffa81da20a1ee604463da062ac9dccadf52d67b007b2752282c58a4fc94445d4cc51dbbb72b2b275'
